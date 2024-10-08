82f82a868d2e9270f26748d4f189de74d05e0c5e
Christian Pabon <christian75876@gmail.com>
2024-10-07 20:58:06 -0500

    refactor: enhance registration module and integrate Swagger documentation

--- last-filter/src/modules/result/result.controller.ts
+++ last-filter/src/modules/result/result.controller.ts
@@ -1,16 +1,29 @@
-import { Controller, Post, Body, Param, Get } from '@nestjs/common';
+import { Controller, Post, Body, Param, Get, UseGuards } from '@nestjs/common';
 import { ResultService } from './result.service';
 import { CreateResultDto } from './dto/create-result.dto';
+import { ApiPostOperation } from 'src/common/decorators/swagger';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from '../auth/guards/jwt.auth.guard';
+import { RolesGuard } from 'src/common/guards/roles.guard';
 
+@ApiTags('result')
 @Controller('results')
+@UseGuards(JwtAuthGuard, RolesGuard)
 export class ResultController {
   constructor(private readonly resultService: ResultService) {}
 
+  @ApiPostOperation('/results', CreateResultDto, CreateResultDto, true)
   @Post()
   create(@Body() createResultDto: CreateResultDto) {
     return this.resultService.createResult(createResultDto);
   }
 
+  @ApiOperation({ summary: 'Get all results for a specific tournament' })
+  @ApiResponse({
+    status: 200,
+    description: 'List of results for the tournament.',
+  })
+  @ApiResponse({ status: 404, description: 'Tournament not found.' })
   @Get('tournaments/:id')
   findTournament(@Param('id') id: number) {
     return this.resultService.findAllResultsByTournament(id);
--- last-filter/src/modules/tournament/dto/create-tournament.dto.ts
+++ last-filter/src/modules/tournament/dto/create-tournament.dto.ts
@@ -4,20 +4,28 @@ import {
   IsDateString,
   IsOptional,
 } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateTournamentDto {
+  @ApiProperty({ description: 'Name of the tournament' })
   @IsNotEmpty()
   @IsString()
   name: string;
 
+  @ApiProperty({ description: 'Start date of the tournament in ISO format' })
   @IsNotEmpty()
   @IsDateString()
   startDate: Date;
 
+  @ApiProperty({ description: 'End date of the tournament in ISO format' })
   @IsNotEmpty()
   @IsDateString()
   endDate: Date;
 
+  @ApiProperty({
+    description: 'Description of the tournament',
+    required: false,
+  })
   @IsOptional()
   @IsString()
   description?: string;
